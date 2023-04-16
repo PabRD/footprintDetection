8d4312120a88bcc7c8317761bd165b1e1a4b756e
PabRD <125137436+PabRD@users.noreply.github.com>
2023-04-16 20:46:24 +0200

    Add files via upload

--- archIndexDetection.m
+++ archIndexDetection.m
@@ -24,19 +24,19 @@ for i = 1:length(ds.Files)
     % imshow(data)
 
     %% Pre Processing
-    % close all
-    % f= figure;
-    % f.WindowState = 'maximized';
-    % t =tiledlayout(2,3);
-    %
-    % nexttile
-    % imshow(data);
-    % title('Original Image', 'Interpreter', 'latex');
-    %
-    % nexttile([1 2])
-    % histogram(data(data>0), 256);
-    % grid on;
-    % title('Histogram of Image Gray Levels', 'Interpreter', 'latex');
+    close all
+    f= figure;
+    f.WindowState = 'maximized';
+    t =tiledlayout(2,3);
+    
+    nexttile
+    imshow(data);
+    title('Original Image', 'Interpreter', 'latex');
+    
+    nexttile([1 2])
+    histogram(data(data>0), 256);
+    grid on;
+    title('Histogram of Image Gray Levels', 'Interpreter', 'latex');
 
 
 
@@ -48,41 +48,41 @@ for i = 1:length(ds.Files)
     amount = 0.4;   % 0.4  a la base
     AdjGrayIMG = localcontrast(grayIMG, edgeThreshold, amount);
 
-    % contrastIM = imadjust(AdjGrayIMG);
-    % nexttile
-    % imshow(contrastIM)
-    % I = contrastIM;
-    % title('Contrasted image', 'Interpreter', 'latex');
-    % [counts,x] = imhist(I,16);
+    contrastIM = imadjust(AdjGrayIMG);
+    nexttile
+    imshow(contrastIM)
+    I = contrastIM;
+    title('Contrasted image', 'Interpreter', 'latex');
+    [counts,x] = imhist(I,16);
 
-    % OtsuMask = otsuthresh(counts);
-    % BW = imbinarize(I,OtsuMask);
-    % nexttile
-    % imshow(BW)
-    % title('Otsu Thresholding Method', 'Interpreter', 'latex');
+    OtsuMask = otsuthresh(counts);
+    BW = imbinarize(I,OtsuMask);
+    nexttile
+    imshow(BW)
+    title('Otsu Thresholding Method', 'Interpreter', 'latex');
 
 
     BW = imbinarize(AdjGrayIMG,"adaptive","ForegroundPolarity","dark");
-    % nexttile
-    % imshow(BW)
-    % title('Bradley Thresholding Method', 'Interpreter', 'latex');
-    %
-    % t.TileSpacing = 'compact';
-    % t.Padding = 'compact';
+    nexttile
+    imshow(BW)
+    title('Bradley Thresholding Method', 'Interpreter', 'latex');
+    
+    t.TileSpacing = 'compact';
+    t.Padding = 'compact';
 
     %% Erode, Dilate and smooth
-    % f2 = figure;
-    % f2.WindowState = 'maximized';
-    % t2 = tiledlayout(2,4);
+    f2 = figure;
+    f2.WindowState = 'maximized';
+    t2 = tiledlayout(2,4);
 
     windowSize = 20;
     se = zeros(windowSize,windowSize);
     se(ceil(windowSize/2), :) = 1;
     se(:, ceil(windowSize/2)) = 1;
     mask = imerode(BW, se);                                                     % Erode
-    % nexttile
-    % imshow(mask)
-    % title('Erode','Interpreter','latex')
+    nexttile
+    imshow(mask)
+    title('Erode','Interpreter','latex')
 
     windowSize = 25;
     se = zeros(windowSize,windowSize);
@@ -90,16 +90,16 @@ for i = 1:length(ds.Files)
     se(:, ceil(windowSize/2)) = 1;
     mask = imdilate(mask, se);                                                  % Dilate
     mask = imclearborder(not(mask));                                            % Clear borders
-    % nexttile
-    % imshow(mask)
-    % title('dilate and clear borders','Interpreter','latex')
+    nexttile
+    imshow(mask)
+    title('dilate and clear borders','Interpreter','latex')
 
 
     % Fill in the mask.
     mask = imfill(mask, 'holes');
-    % nexttile
-    % imshow(mask)
-    % title('fill holes','Interpreter','latex')
+    nexttile
+    imshow(mask)
+    title('fill holes','Interpreter','latex')
 
     % Smooth it some more.
     windowSize = 21;            % 21 de base
@@ -115,9 +115,9 @@ for i = 1:length(ds.Files)
     mask = ~imerode(~mask,se);
 
     mask = imfill(mask, 'holes');
-    % nexttile
-    % imshow(mask)
-    % title('smooth','Interpreter','latex')
+    nexttile
+    imshow(mask)
+    title('smooth','Interpreter','latex')
 
     %% Removing what's not feet
 
@@ -136,17 +136,17 @@ for i = 1:length(ds.Files)
         finalImage = twoObjects;
     end
 
-    % nexttile
-    % imshow(finalImage)
-    % hold on
-    % title('keep biggest forms','Interpreter','latex')
-    % info = regionprops(finalImage,'Boundingbox') ;
-    % for k = 1 : length(info)
-    %      BB = info(k).BoundingBox;
-    %      rectangle('Position', [BB(1),BB(2),BB(3),BB(4)],'EdgeColor','r','LineWidth',2) ;
-    %      xCentroid{k} = BB(1) + BB(3)/2;
-    %      yCentroid{k} = BB(2) + BB(4)/2;
-    % end
+    nexttile
+    imshow(finalImage)
+    hold on
+    title('keep biggest forms','Interpreter','latex')
+    info = regionprops(finalImage,'Boundingbox') ;
+    for k = 1 : length(info)
+         BB = info(k).BoundingBox;
+         rectangle('Position', [BB(1),BB(2),BB(3),BB(4)],'EdgeColor','r','LineWidth',2) ;
+         xCentroid{k} = BB(1) + BB(3)/2;
+         yCentroid{k} = BB(2) + BB(4)/2;
+    end
 
     %% Watershed transform to remove toes
     D = -bwdist(~finalImage);
@@ -158,12 +158,12 @@ for i = 1:length(ds.Files)
 
     bw3 = finalImage;
     bw3(Ld2 == 0) = 0;
-    % nexttile
-    % imshow(bw3)
-    % title('Watershed segmented image','Interpreter','latex')
-    % hold on
-    % himage = imshow(Lrgb);
-    % himage.AlphaData = 0.3;
+    nexttile
+    imshow(bw3)
+    title('Watershed segmented image','Interpreter','latex')
+    hold on
+    himage = imshow(Lrgb);
+    himage.AlphaData = 0.3;
 
     % above image middle we detect small objects (toes)
     midImage = size(bw3,1)/2-200;
@@ -190,37 +190,37 @@ for i = 1:length(ds.Files)
 
     newImage = bwareaopen(newImage,400);                                        % remove last small objects
 
-    % nexttile
-    % imshow(newImage)
-    % title('final footprint ready to process','Interpreter','latex')
+    nexttile
+    imshow(newImage)
+    title('final footprint ready to process','Interpreter','latex')
 
     %% Plot footprint edge on original image
-    % nexttile
-    % imshow(data)
-    % boundaries = bwboundaries(newImage);
-    % numberOfBoundaries = size(boundaries, 1);
-    % hold on;
-    % for k = 1 : numberOfBoundaries
-    %     thisBoundary = boundaries{k};
-    %     x = thisBoundary(:,2);
-    %     y = thisBoundary(:,1);
-    %     plot(x, y, 'r-', 'LineWidth', 2);
-    % end
-    % hold off;
-    % caption = sprintf('Original image and its %d edges', numberOfBoundaries);
-    % title(caption,'Interpreter','latex');
-    % axis('on', 'image');
-    % t2.Padding ="compact";
-    % t2.TileSpacing = "compact";
+    nexttile
+    imshow(data)
+    boundaries = bwboundaries(newImage);
+    numberOfBoundaries = size(boundaries, 1);
+    hold on;
+    for k = 1 : numberOfBoundaries
+        thisBoundary = boundaries{k};
+        x = thisBoundary(:,2);
+        y = thisBoundary(:,1);
+        plot(x, y, 'r-', 'LineWidth', 2);
+    end
+    hold off;
+    caption = sprintf('Original image and its %d edges', numberOfBoundaries);
+    title(caption,'Interpreter','latex');
+    axis('on', 'image');
+    t2.Padding ="compact";
+    t2.TileSpacing = "compact";
 
     %% Arch index (AI) Cavagnah et al., 1987
     % AI = B / (A + B + C)
     % Interpretation: (Chu et al., 1995) Arch Index <0.17 high | >0.25 low
 
-    %figure
+    figure
     AIdata = logical(newImage);
-    %title('Foot divided in three parts to calculate Arch Index')
-    %hold on
+    title('Foot divided in three parts to calculate Arch Index')
+    hold on
     
     %area of 2 differents objects
     footprintProps = regionprops(AIdata,'basic');
@@ -271,25 +271,29 @@ for i = 1:length(ds.Files)
     %% visualisation / debug
     prepVisuPied = AIDeuxTiers{1}+AIDeuxTiers{2};
     tiersrgb = label2rgb(AIdata+prepVisuPied,[0.4 0.4 0.4;0.2 0.2 0.2],'k','noshuffle');
-%     maskTiers = imshow(tiersrgb);
-%     maskTiers.AlphaData = 0.6;
+    maskTiers = imshow(tiersrgb);
+    maskTiers.AlphaData = 0.6;
 
     captionAI1 = sprintf('Arch Index = %1.2f ', archIndex{1});
     captionAI2 = sprintf('Arch Index = %1.2f ', archIndex{2});
 
-%     text(meanXcoordBaseRectangle(1),200,captionAI1,'Color','w','Interpreter','latex','FontWeight','bold','FontSize',10)
-%     text(meanXcoordBaseRectangle(2),200,captionAI2,"Color",'w','Interpreter','latex','FontWeight','bold','FontSize',10)
+    text(meanXcoordBaseRectangle(1),200,captionAI1,'Color','w','Interpreter','latex','FontWeight','bold','FontSize',10)
+    text(meanXcoordBaseRectangle(2),200,captionAI2,"Color",'w','Interpreter','latex','FontWeight','bold','FontSize',10)
 
-%     [xCentroid,yCentroid] = deal(cell(1,length(globalArea)));
-%     for k = 1 : length(globalArea)
-%         BB = footprintProps(k).BoundingBox;
-%         rectangle('Position', [BB(1),BB(2),BB(3),BB(4)],'EdgeColor','r','LineWidth',2) ;
-%         xCentroid{k} = BB(1) + BB(3)/2;
-%         yCentroid{k} = BB(2) + BB(4)/2;
-%     end
+    [xCentroid,yCentroid] = deal(cell(1,length(globalArea)));
+    for k = 1 : length(globalArea)
+        BB = footprintProps(k).BoundingBox;
+        rectangle('Position', [BB(1),BB(2),BB(3),BB(4)],'EdgeColor','r','LineWidth',2) ;
+        xCentroid{k} = BB(1) + BB(3)/2;
+        yCentroid{k} = BB(2) + BB(4)/2;
+    end
 
 result(i).AIleft = archIndex{1};
 result(i).AIright = archIndex{2};
+
+msgbox('Press any key to continue')
+pause
+close all
 
 end
 toc
